8cd951698caf169eef11ee90521a8fc99ff3bb65
Abby Yuan <abbymengyuan@gmail.com>
2025-05-08 16:27:49 +1000

    release version 0.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
 
 setup(
     name="topic-specificity",
-    version="0.1.0",
+    version="0.1.1",
     author="Meng Yuan",
     author_email="meng.yuan@unimelb.edu.au",
     maintainer="Meng Yuan",
