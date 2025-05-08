c7c770df936b4e1d44f18d6080d37cbb649a34db
Abby Yuan <abbymengyuan@gmail.com>
2025-05-08 15:51:58 +1000

    fixed python version support in workflow

--- setup.py
+++ setup.py
@@ -1,15 +1,32 @@
 from setuptools import setup, find_packages
+import os
+
+# Read the contents of your README file
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
-    name="topic_specificity",
+    name="topic-specificity",
     version="0.1.0",
-    description="A Python package for calculating topic specificity for LDA, HDP, and LSA models.",
     author="Meng Yuan",
+    author_email="meng.yuan@unimelb.edu.au",
+    maintainer="Meng Yuan",
+    maintainer_email="meng.yuan@unimelb.edu.au",
+    description="Calculate topic specificity for LDA, LSA & HDP.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/abigailyuan/topic_specificity",
+    project_urls={
+        "Paper": "https://doi.org/10.1145/3578337.3605118",
+        "CITATION": "https://github.com/abigailyuan/topic_specificity/blob/main/CITATION.cff"
+    },
     packages=find_packages(),
-    install_requires=[
-        "numpy",
-        "scikit-learn"
-        # add 'gensim' if you're using gensim models
+    install_requires=["numpy", "scikit-learn"],
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
 )
